e71b316c197729af4e9ee76c32c3710a279da5a0
huanghanbin <hhb121514@163.com>
2024-12-25 11:21:40 +0800

    更新

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,10 @@
 <template>
   <div class="layout-page ignore-max-width">
     <slot />
-    <van-tabbar v-model="active">
-      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item to="/about" icon="search">关于</van-tabbar-item>
-      <van-tabbar-item to="/profile" icon="friends-o">个人</van-tabbar-item>
-    </van-tabbar>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from "vue"
-
-const active = ref(0)
-
-const route = useRoute()
-const ROUTE_LIST = ["/", "/about", "profile"]
-
-watch(
-  () => route.path,
-  (newVal) => {
-    ROUTE_LIST.forEach((item, index) => {
-      // 匹配当前路径，获取索引
-      if (item === newVal) {
-        active.value = index
-      }
-    })
-  },
-  { immediate: true }
-)
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
 .layout-page {
--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,23 @@
     <div>首页</div>
     <h1>{{ res.data.msg }}</h1>
     <h2>{{ res.data.name }}</h2>
+    <van-button type="primary" @click="btnClick">主要按钮</van-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { getAboutMessage } from "@/service/about"
+import { showSuccessToast } from "vant"
 
 useHead({
   title: "首页"
 })
 
 const res = await getAboutMessage()
+
+const btnClick = () => {
+  showSuccessToast("Success")
+}
 </script>
 
 <style scoped lang="scss"></style>
