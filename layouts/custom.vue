e695cea395f6f415090fba4d6e0f17053fcec383
huanghanbin <hhb121514@163.com>
2024-12-25 11:32:06 +0800

    更新

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
+  <div class="common-page ignore-max-width">
     <slot />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
+<style lang="scss" scoped>
+.common-page {
+  width: 750px;
+  padding-bottom: 120px;
+  margin: 0 auto;
+  overflow: hidden;
+}
 
-<style>
-
+.ignore-max-width {
+  max-width: 768px;
+}
 </style>
