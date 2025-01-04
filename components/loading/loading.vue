aa2337b36cf3923c09362276ee7ce3f47def0c40
huanghanbin <hhb121514@163.com>
2025-01-04 17:27:21 +0800

    更新

---
+++ components/loading/loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading-box">
+    <div class="loading">
+      <van-loading color="#1989fa" vertical>加载中...</van-loading>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.loading-box {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 999;
+
+  .loading {
+    position: absolute;
+    left: 50%;
+    top: 30%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
